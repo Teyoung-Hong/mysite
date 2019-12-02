af7e1dfe56bec9f0f85c7b28d47021d43b67170c
Teyoung Hong <teyon23@icloud.com>
2019-12-02 18:51:13 +0900

    [update] 自己紹介の簡単な部分を

--- page-about.php
+++ page-about.php
@@ -17,10 +17,59 @@ get_header();
   </div>
   <div class="about__main">
     <div class="about__nav">
-      <ul class="">
-      
+      <ul class="about__nav--items">
+        <li class="about__nav--item">INTRO</li>
+        <li class="about__nav--item">WORK</li>
+        <li class="about__nav--item">SKILL</li>
+        <li class="about__nav--item">MEDIA</li>
+        <li class="about__nav--item">CONTACT</li>
       </ul>
-    </div> 
+    </div>
+    <div class="about__content intro is_active">
+      <h2 class="about__content--title">Introduction</h2>
+      <div class="about__content--body">
+        <p class="about__content--subtitle">私は...</p>
+        <p class="about__content--sentence">多動症。</p>
+      </div>
+    </div>
+    <div class="about__content work">
+      <h2 class="about__content--title">Work</h2>
+      <div class="about__content--body">
+        <p class="about__content--subtitle">Engneer</p>
+        <p class="about__content--sentence">
+        フリーランスでエンジニアをしています。主にコーポレートサイトやランディングページの開発を請け負っています。<br>成果物は諸事情によりここでは表示できませんが、このウェブサイトは僕が作っています。
+        </p>
+        <p class="about__content--subtitle">使用言語（このサイト）</p>
+        <p class="about__content--sentence">HTML / CSS / Java Script / Jquery / PHP</p>
+      </div>
+    </div>
+    <div class="about__content skill">
+      <h2 class="about__content--title">Skill</h2>
+      <div class="about__content--body">
+        <p class="about__content--subtitle">Programming</p>
+        <p class="about__content--sentence">HTML / CSS / Java Script / Jquery / React / Ruby (Ruby on Rails) / Python (Django)</p>
+        <p class="about__content--subtitle">Language</p>
+        <p class="about__content--sentence">English (toeic 850) / Chinese (HSK 6級) / Korean (Native) / Japanese (漢検 5級...)</p>
+        <p class="about__content--subtitle">Else</p>
+        <p class="about__content--sentence">すべらない話(笑率90%)</p>
+      </div>
+    </div>
+    <div class="about__content media">
+      <h2 class="about__content--title">Media</h2>
+      <div class="about__content--body">
+        <p class="about__content--subtitle">Web media</p>
+        <p class="about__content--sentence">プログラミングの始め方や進め方、言語の習得方法、哲学らへんのことをバリバリに発信。</p>
+        <p class="about__content--subtitle">Youtube</p>
+        <p class="about__content--sentence">プログラミングやアプリ開発、哲学に関してのことなどに関する動画をアップしてます。</p>
+      </div>
+    </div>
+    <div class="about__content contact">
+      <h2 class="about__content--title">Contact</h2>
+      <div class="about__content--body">
+        <p class="about__content--subtitle">コンタクトフォーム 作成中</p>
+        <p class="about__content--sentence">テレパシーでお願いします。</p>
+      </div>
+    </div>
   </div>
 </div>
 
