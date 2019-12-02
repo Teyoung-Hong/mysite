5c42faa65deea740f339650ce19589b3946a770a
Teyoung Hong <teyon23@icloud.com>
2019-12-02 20:33:59 +0900

    [update] about page was updated except layout

--- page-about.php
+++ page-about.php
@@ -17,22 +17,20 @@ get_header();
   </div>
   <div class="about__main">
     <div class="about__nav">
-      <ul class="about__nav--items">
-        <li class="about__nav--item">INTRO</li>
-        <li class="about__nav--item">WORK</li>
-        <li class="about__nav--item">SKILL</li>
-        <li class="about__nav--item">MEDIA</li>
-        <li class="about__nav--item">CONTACT</li>
-      </ul>
+      <li class="about__nav--item nav__intro">INTRO</li>
+      <li class="about__nav--item nav__work">WORK</li>
+      <li class="about__nav--item nav__skill">SKILL</li>
+      <li class="about__nav--item nav__media">MEDIA</li>
+      <li class="about__nav--item nav__contact">CONTACT</li>
     </div>
-    <div class="about__content intro is_active">
+    <div id="intro" class="about__content intro is_active">
       <h2 class="about__content--title">Introduction</h2>
       <div class="about__content--body">
         <p class="about__content--subtitle">私は...</p>
         <p class="about__content--sentence">多動症。</p>
       </div>
     </div>
-    <div class="about__content work">
+    <div id="work" class="about__content work">
       <h2 class="about__content--title">Work</h2>
       <div class="about__content--body">
         <p class="about__content--subtitle">Engneer</p>
@@ -43,7 +41,7 @@ get_header();
         <p class="about__content--sentence">HTML / CSS / Java Script / Jquery / PHP</p>
       </div>
     </div>
-    <div class="about__content skill">
+    <div id="skill" class="about__content skill">
       <h2 class="about__content--title">Skill</h2>
       <div class="about__content--body">
         <p class="about__content--subtitle">Programming</p>
@@ -54,7 +52,7 @@ get_header();
         <p class="about__content--sentence">すべらない話(笑率90%)</p>
       </div>
     </div>
-    <div class="about__content media">
+    <div id="media" class="about__content media">
       <h2 class="about__content--title">Media</h2>
       <div class="about__content--body">
         <p class="about__content--subtitle">Web media</p>
@@ -63,7 +61,7 @@ get_header();
         <p class="about__content--sentence">プログラミングやアプリ開発、哲学に関してのことなどに関する動画をアップしてます。</p>
       </div>
     </div>
-    <div class="about__content contact">
+    <div id="contact" class="about__content contact">
       <h2 class="about__content--title">Contact</h2>
       <div class="about__content--body">
         <p class="about__content--subtitle">コンタクトフォーム 作成中</p>
@@ -72,6 +70,10 @@ get_header();
     </div>
   </div>
 </div>
+
+<style>
+header{display:none;}
+</style>
 
   <div class=""></div>
 <?php get_footer(); ?>
